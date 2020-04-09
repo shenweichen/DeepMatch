f150d1470746168a93584b4edc3b5be316f35fa4
shenweichen <wcshen1994@163.com>
2020-04-09 17:06:22 +0800

    no message

--- deepmatch/models/youtubednn.py
+++ deepmatch/models/youtubednn.py
@@ -9,7 +9,7 @@ from deepctr.inputs import input_from_feature_columns, build_input_features, com
 from deepctr.layers.core import DNN
 from deepctr.layers.utils import NoMask
 from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Input
+from tensorflow.python.keras.layers import Input,Lambda
 
 from deepmatch.utils import get_item_embedding,get_item_embeddingv2
 from ..inputs import input_from_feature_columns
@@ -41,7 +41,7 @@ def YoutubeDNN(user_feature_columns, item_feature_columns, num_sampled=5,
         raise ValueError("Now YoutubeNN only support 1 item feature like item_id")
     item_feature_name = item_feature_columns[0].name
     item_vocabulary_size = item_feature_columns[0].vocabulary_size
-    item_idx = Input(shape=[None,],tensor=tf.range(0,item_vocabulary_size),name="item_idx")
+    #item_idx = Input(shape=[None,],tensor=tf.range(0,item_vocabulary_size),name="item_idx")
 
     embedding_matrix_dict = create_embedding_matrix(user_feature_columns + item_feature_columns, l2_reg_embedding,
                                                     init_std, seed, prefix="")
@@ -59,7 +59,11 @@ def YoutubeDNN(user_feature_columns, item_feature_columns, num_sampled=5,
     user_dnn_out = DNN(user_dnn_hidden_units, dnn_activation, l2_reg_dnn, dnn_dropout,
                        dnn_use_bn, seed, )(user_dnn_input)
 
-    item_embedding = NoMask()(embedding_matrix_dict[item_feature_name](item_idx))
+    #item_embedding = NoMask()(embedding_matrix_dict[item_feature_name](item_idx))
+    item_embedding = embedding_matrix_dict[item_feature_name]#Lambda( lambda x:embedding_matrix_dict[item_feature_name])(None)
+    # if not item_embedding.built:
+    #     item_embedding.build()
+    item_embedding = Input(tensor=item_embedding.embeddings,name=item_feature_name+"_embeddings")
     #item_embedding = get_item_embeddingv2()
 
     # if not item_embedding.built:
@@ -67,7 +71,7 @@ def YoutubeDNN(user_feature_columns, item_feature_columns, num_sampled=5,
 
     output = SampledSoftmaxLayerv2( num_sampled=num_sampled)(
         inputs=(item_embedding,user_dnn_out, item_features[item_feature_name]))
-    model = Model(inputs=user_inputs_list + item_inputs_list+[item_idx], outputs=output)
+    model = Model(inputs=user_inputs_list + item_inputs_list+[item_embedding], outputs=output)#+[item_idx]
 
     model.__setattr__("user_input", user_inputs_list)
     model.__setattr__("user_embedding", user_dnn_out)
