f30aba9d8a1d23e0b010c6c6ab67d57aae8fd8dd
unknown <wangzhe449@360buyad.local>
2020-05-16 14:58:52 +0800

    update sdm

--- examples/run_sdm_sampledsoftmax.py
+++ examples/run_sdm_sampledsoftmax.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from deepctr.inputs import SparseFeat, VarLenSparseFeat
-from examples.preprocess import gen_data_set, gen_model_input_sdm
+from examples.preprocess import gen_data_set_sdm, gen_model_input_sdm
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.models import Model
+from tensorflow.python.keras import optimizers
 
 from deepmatch.models import SDM
 from deepmatch.utils import sampledsoftmaxloss
@@ -20,29 +21,28 @@ if __name__ == "__main__":
     data = pd.merge(pd.merge(ratings, movies), user)  # .iloc[:10000]
 
     sparse_features = ["movie_id", "user_id",
-                       "gender", "age", "occupation", "zip", ]
-    SEQ_LEN_short = 10
+                       "gender", "age", "occupation", "zip", "genres"]
+    SEQ_LEN_short = 5
     SEQ_LEN_prefer = 50
-    negsample = 0
 
     # 1.Label Encoding for sparse features,and process sequence features with `gen_date_set` and `gen_model_input`
 
-    features = ['user_id', 'movie_id', 'gender', 'age', 'occupation', 'zip']
+    features = ['user_id', 'movie_id', 'gender', 'age', 'occupation', 'zip', 'genres']
     feature_max_idx = {}
     for feature in features:
         lbe = LabelEncoder()
         data[feature] = lbe.fit_transform(data[feature]) + 1
         feature_max_idx[feature] = data[feature].max() + 1
 
-    user_profile = data[["user_id", "gender", "age", "occupation", "zip"]].drop_duplicates('user_id')
+    user_profile = data[["user_id", "gender", "age", "occupation", "zip", "genres"]].drop_duplicates('user_id')
 
     item_profile = data[["movie_id"]].drop_duplicates('movie_id')
 
     user_profile.set_index("user_id", inplace=True)
+    #
+    # user_item_list = data.groupby("user_id")['movie_id'].apply(list)
 
-    user_item_list = data.groupby("user_id")['movie_id'].apply(list)
-
-    train_set, test_set = gen_data_set(data, negsample)
+    train_set, test_set = gen_data_set_sdm(data, seq_short_len=SEQ_LEN_short, seq_prefer_len=SEQ_LEN_prefer)
 
     train_model_input, train_label = gen_model_input_sdm(train_set, user_profile, SEQ_LEN_short, SEQ_LEN_prefer)
     test_model_input, test_label = gen_model_input_sdm(test_set, user_profile, SEQ_LEN_short, SEQ_LEN_prefer)
@@ -57,9 +57,13 @@ if __name__ == "__main__":
                             SparseFeat("occupation", feature_max_idx['occupation'], 16),
                             SparseFeat("zip", feature_max_idx['zip'], 16),
                             VarLenSparseFeat(SparseFeat('short_movie_id', feature_max_idx['movie_id'], embedding_dim,
-                                                        embedding_name="movie_id"), SEQ_LEN_short, 'mean', 'prefer_sess_length'),
+                                                        embedding_name="movie_id"), SEQ_LEN_short, 'mean', 'short_sess_length'),
                             VarLenSparseFeat(SparseFeat('prefer_movie_id', feature_max_idx['movie_id'], embedding_dim,
-                                                        embedding_name="movie_id"), SEQ_LEN_prefer, 'mean', 'short_sess_length'),
+                                                        embedding_name="movie_id"), SEQ_LEN_prefer, 'mean', 'prefer_sess_length'),
+                            VarLenSparseFeat(SparseFeat('short_genres', feature_max_idx['genres'], embedding_dim,
+                                                        embedding_name="genres"), SEQ_LEN_short, 'mean', 'short_sess_length'),
+                            VarLenSparseFeat(SparseFeat('prefer_genres', feature_max_idx['genres'], embedding_dim,
+                                                        embedding_name="genres"), SEQ_LEN_prefer, 'mean', 'prefer_sess_length'),
                             ]
 
     item_feature_columns = [SparseFeat('movie_id', feature_max_idx['movie_id'], embedding_dim)]
@@ -71,9 +75,11 @@ if __name__ == "__main__":
     if tf.__version__ >= '2.0.0':
         tf.compat.v1.disable_eager_execution()
 
-    model = SDM(user_feature_columns, item_feature_columns, history_feature_list=['movie_id'], units=embedding_dim, num_sampled=100,)
+    # units must be equal to item embedding dim!
+    model = SDM(user_feature_columns, item_feature_columns, history_feature_list=['movie_id', 'genres'], units=embedding_dim, num_sampled=100,)
 
-    model.compile(optimizer="adam", loss=sampledsoftmaxloss)  # "binary_crossentropy")
+    optimizer = optimizers.Adam(lr=0.001, clipnorm=5.0)
+    model.compile(optimizer=optimizer, loss=sampledsoftmaxloss)  # "binary_crossentropy")
     model.summary()
     history = model.fit(train_model_input, train_label,  # train_label,
                         batch_size=512, epochs=20, verbose=1, validation_split=0.0, )
