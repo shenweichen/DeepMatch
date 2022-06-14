ce06f4e188677cc0f02b85bfdd5775be63a750b1
浅梦 <weichen.swc@alibaba-inc.com>
2022-06-15 01:37:39 +0800

    no message

--- tests/models/SDM_test.py
+++ tests/models/SDM_test.py
@@ -9,9 +9,9 @@ from ..utils import check_model, get_xy_fd_sdm
 
 def test_SDM():
     model_name = "SDM"
-    tf.keras.backend.set_learning_phase(1)
+    #tf.keras.backend.set_learning_phase(1)
     x, y, user_feature_columns, item_feature_columns, history_feature_list = get_xy_fd_sdm(False)
-    K.set_learning_phase(True)
+    #K.set_learning_phase(True)
 
     if tf.__version__ >= '2.0.0':
         tf.compat.v1.disable_eager_execution()
