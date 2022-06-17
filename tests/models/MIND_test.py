ed8405e37010ccc4e35ae76ef46080b647c3e885
浅梦 <weichen.swc@alibaba-inc.com>
2022-06-17 12:27:42 +0800

    no message

--- tests/models/MIND_test.py
+++ tests/models/MIND_test.py
@@ -20,7 +20,8 @@ def test_MIND(dynamic_k, p):
 
     if tf.__version__ >= '2.0.0':
         tf.compat.v1.disable_eager_execution()
-
+    else:
+        K.set_learning_phase(True)
     model = MIND(user_feature_columns, item_feature_columns, num_sampled=2, p=p, dynamic_k=dynamic_k,
                  user_dnn_hidden_units=(16, 4))
 
