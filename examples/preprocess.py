7ef2028c7ad2df0bc5158a1a19a7e5ec3b0191a9
浅梦 <weichen.swc@alibaba-inc.com>
2022-07-03 14:14:46 +0800

    14

--- examples/preprocess.py
+++ examples/preprocess.py
@@ -34,7 +34,7 @@ def gen_data_set(data, negsample=0):
     return train_set, test_set
 
 
-def gen_data_set_sdm(data, seq_short_len=5, seq_prefer_len=50):
+def gen_data_set_sdm(data, seq_short_max_len=5, seq_prefer_max_len=50):
     data.sort_values("timestamp", inplace=True)
     train_set = []
     test_set = []
@@ -45,22 +45,35 @@ def gen_data_set_sdm(data, seq_short_len=5, seq_prefer_len=50):
         for i in range(1, len(pos_list)):
             hist = pos_list[:i]
             genres_hist = genres_list[:i]
-            if i <= seq_short_len and i != len(pos_list) - 1:
-                train_set.append((reviewerID, hist[::-1], [0] * seq_prefer_len, pos_list[i], 1, len(hist[::-1]), 0,
-                                  rating_list[i], genres_hist[::-1], [0] * seq_prefer_len))
-            elif i != len(pos_list) - 1:
+            seq_short_len = min(i, seq_short_max_len)
+            seq_prefer_len = max(i - seq_short_len, 0)
+            if i != len(pos_list) - 1:
                 train_set.append(
-                    (reviewerID, hist[::-1][:seq_short_len], hist[::-1][seq_short_len:], pos_list[i], 1, seq_short_len,
-                     len(hist[::-1]) - seq_short_len, rating_list[i], genres_hist[::-1][:seq_short_len],
+                    (reviewerID, hist[::-1][:seq_short_len][::-1], hist[::-1][seq_short_len:], pos_list[i], 1, seq_short_len,
+                     seq_prefer_len, rating_list[i], genres_hist[::-1][:seq_short_len][::-1],
                      genres_hist[::-1][seq_short_len:]))
-            elif i <= seq_short_len and i == len(pos_list) - 1:
-                test_set.append((reviewerID, hist[::-1], [0] * seq_prefer_len, pos_list[i], 1, len(hist[::-1]), 0,
-                                 rating_list[i], genres_hist[::-1], [0] * seq_prefer_len))
             else:
                 test_set.append(
-                    (reviewerID, hist[::-1][:seq_short_len], hist[::-1][seq_short_len:], pos_list[i], 1, seq_short_len,
-                     len(hist[::-1]) - seq_short_len, rating_list[i], genres_hist[::-1][:seq_short_len],
+                    (reviewerID, hist[::-1][:seq_short_len][::-1], hist[::-1][seq_short_len:], pos_list[i], 1, seq_short_len,
+                     seq_prefer_len, rating_list[i], genres_hist[::-1][:seq_short_len][::-1],
                      genres_hist[::-1][seq_short_len:]))
+
+            # if i <= seq_short_max_len and i != len(pos_list) - 1: # i <= seq_short_max_len & i != len()-1
+            #     train_set.append((reviewerID, hist[::-1], [0] * seq_prefer_max_len, pos_list[i], 1, len(hist[::-1]), 0,
+            #                       rating_list[i], genres_hist[::-1], [0] * seq_prefer_max_len))
+            # elif i != len(pos_list) - 1: # i>seq_short_max_len & i != len() - 1
+            #     train_set.append(
+            #         (reviewerID, hist[::-1][:seq_short_max_len], hist[::-1][seq_short_max_len:], pos_list[i], 1, seq_short_max_len,
+            #          len(hist[::-1]) - seq_short_max_len, rating_list[i], genres_hist[::-1][:seq_short_max_len],
+            #          genres_hist[::-1][seq_short_max_len:]))
+            # elif i <= seq_short_max_len and i == len(pos_list) - 1: #i <= seq_short_max_len & i == len()-1
+            #     test_set.append((reviewerID, hist[::-1], [0] * seq_prefer_max_len, pos_list[i], 1, len(hist[::-1]), 0,
+            #                      rating_list[i], genres_hist[::-1], [0] * seq_prefer_max_len))
+            # else: # i> seq_short_max_len & i== len() - 1
+            #     test_set.append(
+            #         (reviewerID, hist[::-1][:seq_short_max_len], hist[::-1][seq_short_max_len:], pos_list[i], 1, seq_short_max_len,
+            #          len(hist[::-1]) - seq_short_max_len, rating_list[i], genres_hist[::-1][:seq_short_max_len],
+            #          genres_hist[::-1][seq_short_max_len:]))
 
     random.shuffle(train_set)
     random.shuffle(test_set)
@@ -87,7 +100,7 @@ def gen_model_input(train_set, user_profile, seq_max_len):
     return train_model_input, train_label
 
 
-def gen_model_input_sdm(train_set, user_profile, seq_short_len, seq_prefer_len):
+def gen_model_input_sdm(train_set, user_profile, seq_short_max_len, seq_prefer_max_len):
     train_uid = np.array([line[0] for line in train_set])
     short_train_seq = [line[1] for line in train_set]
     prefer_train_seq = [line[2] for line in train_set]
@@ -98,19 +111,19 @@ def gen_model_input_sdm(train_set, user_profile, seq_short_len, seq_prefer_len):
     short_train_seq_genres = np.array([line[8] for line in train_set])
     prefer_train_seq_genres = np.array([line[9] for line in train_set])
 
-    train_short_item_pad = pad_sequences(short_train_seq, maxlen=seq_short_len, padding='post', truncating='post',
+    train_short_item_pad = pad_sequences(short_train_seq, maxlen=seq_short_max_len, padding='post', truncating='post',
                                          value=0)
-    train_prefer_item_pad = pad_sequences(prefer_train_seq, maxlen=seq_prefer_len, padding='post', truncating='post',
+    train_prefer_item_pad = pad_sequences(prefer_train_seq, maxlen=seq_prefer_max_len, padding='post', truncating='post',
                                           value=0)
-    # train_short_genres_pad = pad_sequences(short_train_seq_genres, maxlen=seq_short_len, padding='post', truncating='post',
+    # train_short_genres_pad = pad_sequences(short_train_seq_genres, maxlen=seq_short_max_len, padding='post', truncating='post',
     #                                     value=0)
-    # train_prefer_genres_pad = pad_sequences(prefer_train_seq_genres, maxlen=seq_prefer_len, padding='post', truncating='post',
+    # train_prefer_genres_pad = pad_sequences(prefer_train_seq_genres, maxlen=seq_prefer_max_len, padding='post', truncating='post',
     #                                     value=0)
 
     train_model_input = {"user_id": train_uid, "movie_id": train_iid, "short_movie_id": train_short_item_pad,
-                         "prefer_movie_id": train_prefer_item_pad, "prefer_sess_length": np.minimum(train_prefer_len, seq_prefer_len),
+                         "prefer_movie_id": train_prefer_item_pad, "prefer_sess_length": np.minimum(train_prefer_len, seq_prefer_max_len),
                          "short_sess_length":
-                             np.minimum(train_short_len, seq_short_len)}  # 'short_genres': train_short_genres_pad, 'prefer_genres': train_prefer_genres_pad
+                             np.minimum(train_short_len, seq_short_max_len)}  # 'short_genres': train_short_genres_pad, 'prefer_genres': train_prefer_genres_pad
 
     for key in ["gender", "age", "occupation", "zip"]:
         train_model_input[key] = user_profile.loc[train_model_input['user_id']][key].values
