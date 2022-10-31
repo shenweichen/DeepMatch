4db88662173b0d1c70316c670adb5bd818621357
浅梦 <weichen.swc@alibaba-inc.com>
2022-10-31 19:51:28 +0800

    no message

--- tests/models/SDM_test.py
+++ tests/models/SDM_test.py
@@ -11,8 +11,8 @@ def test_SDM():
     x, y, user_feature_columns, item_feature_columns, history_feature_list = get_xy_fd_sdm(False)
 
     if tf.__version__ >= '2.0.0':
-        # tf.compat.v1.disable_eager_execution()
-        tf.compat.v1.disable_v2_behavior()
+        tf.compat.v1.disable_eager_execution()
+        #tf.compat.v1.disable_v2_behavior()
     else:
         K.set_learning_phase(True)
 
